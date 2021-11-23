7d077002384e9007c523395fe85aea432d4e60a3
firizky29 <13520095@std.stei.itb.ac.id>
2021-11-23 13:55:25 +0700

    CYK dan grammar reader debugging

--- src/CYK-algorithm.py
+++ src/CYK-algorithm.py
@@ -27,39 +27,44 @@ import numpy as np
 def CYK(w: str, lang: dict):
     n = len(w) 
     m = len(lang)
-    dp = np.zeros((m, n, n))
-    id = dict(zip([i for i in range(m)], lang))
-    rule = []
-    i = 1
+    dp = np.zeros((n+1, n+1, m+1))
+    id = dict(zip(lang, [i for i in range(1, m+1)]))
+    rule = [None]
     for var in lang:
         rule.append(lang[var])
-        i+=1
-    for i in range(n):
-        for j in range(m):
+
+    for i in range(1,n+1):
+        for j in range(1,m+1):
             for term in rule[j]:
-                if(term[0]==w[i]):
-                    dp[0][i][j] = True
-    for l in range(1, n):
-        for s in range(n-l+1):
-            for p in range(l-1):
-                for i in range(m):
+                if(term[0]==w[i-1]):
+                    dp[1][i][j] = True
+                    break
+                
+    for l in range(2, n+1):
+        for s in range(1, n-l+2):
+            for p in range(1, l):
+                for i in range(1, m+1):
                     for term in rule[i]:
                         if(len(term)!=1):
-                            id1 = id[term[0]]
-                            id2 = id[term[1]]
-                            dp[l][s][i] = (dp[p][s][id1]&dp[l-p][s+p][id2])
-
-    if(dp[n-1][0][0]):
+                            j = id[term[0]]
+                            k = id[term[1]]
+                            if(dp[p][s][j] and dp[l-p][s+p][k]):
+                                dp[l][s][i] = True
+                                break
+                                
+    if(dp[n][1][1]):
         return -1
     else:
         lines = w.split('\n')
         idLines = dict(zip([i for i in range(len(w)) if w[i]=='\n'], [i for i in range(len(lines))]))
         ret = 1
-        for i in range(n-1, -1, -1):
-            if(dp[i][0][0]):
+        for i in range(n, 0, -1):
+            if(dp[i][1][1]):
                 break
-            if(w[i]=='\n'):
-                ret = idLines[i]
-            return ret
+            if(w[i-1]=='\n'):
+                ret = idLines[i-1]
+        return ret
+
+
 
 
