b3abe7a9a9889c81b670be5005fd04961296d21d
firizky29 <13520095@std.stei.itb.ac.id>
2021-11-24 20:29:09 +0700

    please god, ini final

--- test/tes.py
+++ test/tes.py
@@ -1,12 +1,2 @@
-''' This is a sample multiline comment
-'''
-x + 2 = 3
-if x == 0 + 1
-    return 0
-elif x + 4 == 1:
-    else:
-        return 2
-elif x == 32:
-    return 4
-else:
-    return "Doodoo"
+def func(x,y,zz, z=12, **za):
+    print(y)
